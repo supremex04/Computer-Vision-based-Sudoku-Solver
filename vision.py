c76e350fd72ec09e079e76dff05b854c866e1ea1
supremex04 <84307811+supremex04@users.noreply.github.com>
2023-12-01 19:49:59 +0545

    create vision.py

--- vision.py
+++ vision.py
@@ -2,15 +2,16 @@ import cv2 as cv
 import numpy as np
 from stackwindows import stackImages
 
-path = "C:/Users/bhand/OneDrive/Documents/GitHub/Computer-Vision-based-Sudoku-Solver/pictures/test2.jpg"
+path = "./pictures/test2.jpg"
 
 height = 500
 width = 500
 
 image = cv.imread(path)
 image = cv.resize(image, (width, height))
-grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
+#pre-processing
+grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 blurredImage = cv.GaussianBlur(grayImage,(5,5),1)
 threshold = cv.adaptiveThreshold(blurredImage, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 9,2)
 
@@ -23,3 +24,4 @@ stackedImages =stackImages(0.7, imageList)
 
 cv.imshow("window", stackedImages)
 cv.waitKey(0)
+cv.destroyAllWindows()
