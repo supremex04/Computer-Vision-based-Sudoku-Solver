4daf29abe72a5be85bd20598b9817316b594fdb4
supremex04 <84307811+supremex04@users.noreply.github.com>
2023-12-01 22:11:59 +0545

    contours detection

--- vision.py
+++ vision.py
@@ -15,10 +15,15 @@ grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 blurredImage = cv.GaussianBlur(grayImage,(5,5),1)
 threshold = cv.adaptiveThreshold(blurredImage, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 9,2)
 
+#contour detection
+contours, hierarchy = cv.findContours(threshold, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+image_with_contours = image.copy()
+cv.drawContours(image_with_contours, contours, -1, (0, 255, 0), 2)
+
 blank = np.zeros((height, width,3), np.uint8)
 
 
-imageList = [[image,blurredImage, threshold],[threshold, blank,blank]]
+imageList = [[image,blurredImage, threshold],[threshold, image_with_contours,blank]]
 stackedImages =stackImages(0.7, imageList)
 
 
