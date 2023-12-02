352e8e3a0337adf316fb9dd96266f43b55554945
supremex04 <84307811+supremex04@users.noreply.github.com>
2023-12-02 12:37:50 +0545

    Update vision.py

--- vision.py
+++ vision.py
@@ -18,16 +18,42 @@ threshold = cv.adaptiveThreshold(blurredImage, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_
 #contour detection
 contours, hierarchy = cv.findContours(threshold, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 image_with_contours = image.copy()
-cv.drawContours(image_with_contours, contours, -1, (0, 255, 0), 2)
+#cv.drawContours(image_with_contours, contours, -1, (0, 255, 0), 2)
 
 #largest contour
+def largestContour(contours):
+    maxArea = 0
+    for contour in contours:
+        area = cv.contourArea(contour)
+        if area > 100:
+            perimeter = cv.arcLength(contour, True)
+            approx = cv.approxPolyDP(contour, 0.02*perimeter,True)
+            if (area > maxArea) and len(approx) == 4:
+                largest = contour
+                maxArea = area
+    return largest, maxArea
+
+largest, maxArea = largestContour(contours)
+cv.drawContours(image_with_contours, largest, -1, (0, 255, 0), 5)
+
+print(largest)
+
+#corner points of the bounding rectangle
+x, y, w, h = cv.boundingRect(largest)
+#sourcePoints = np.float32([x,y],[]) 
+largestDetectImage = image.copy()
+cv.circle(largestDetectImage, (x,y), 10, (0,0,255),cv.FILLED)
+cv.circle(largestDetectImage, (x+w,y), 10, (0,0,255),cv.FILLED)
+cv.circle(largestDetectImage, (x,y+h), 10, (0,0,255),cv.FILLED)
+cv.circle(largestDetectImage, (x+w,y+h), 10, (0,0,255),cv.FILLED)
+
 
 
 blank = np.zeros((height, width,3), np.uint8)
 
 
-imageList = [[image,blurredImage, threshold],[image_with_contours, threshold,blank]]
-stackedImages =stackImages(0.7, imageList)
+imageList = [[image,blurredImage, threshold],[image_with_contours, threshold,largestDetectImage]]
+stackedImages =stackImages(0.6, imageList)
 
 
 cv.imshow("window", stackedImages)
