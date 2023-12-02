ae8b2a4cc85a49c5b00ae742f11ce0f9d5df1201
supremex04 <84307811+supremex04@users.noreply.github.com>
2023-12-02 09:35:38 +0545

    sudoku contour detection

--- vision.py
+++ vision.py
@@ -2,7 +2,7 @@ import cv2 as cv
 import numpy as np
 from stackwindows import stackImages
 
-path = "./pictures/test2.jpg"
+path = "./pictures/test4.jpg"
 
 height = 500
 width = 500
@@ -13,17 +13,20 @@ image = cv.resize(image, (width, height))
 #pre-processing
 grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 blurredImage = cv.GaussianBlur(grayImage,(5,5),1)
-threshold = cv.adaptiveThreshold(blurredImage, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 9,2)
+threshold = cv.adaptiveThreshold(blurredImage, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 15,7)
 
 #contour detection
 contours, hierarchy = cv.findContours(threshold, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 image_with_contours = image.copy()
 cv.drawContours(image_with_contours, contours, -1, (0, 255, 0), 2)
 
+#largest contour
+
+
 blank = np.zeros((height, width,3), np.uint8)
 
 
-imageList = [[image,blurredImage, threshold],[threshold, image_with_contours,blank]]
+imageList = [[image,blurredImage, threshold],[image_with_contours, threshold,blank]]
 stackedImages =stackImages(0.7, imageList)
 
 
