55e63ec9678b1037b12629a9ccd1ce47c3fc7041
supremex04 <84307811+supremex04@users.noreply.github.com>
2023-12-09 16:59:09 +0545

    Update recognition.py

--- recognition.py
+++ recognition.py
@@ -2,7 +2,30 @@ import numpy as np
 import cv2 as cv
 from keras.models import load_model
 
-
 def initializeModel():
     model = load_model("./Files/Model/model.h5")
     return model
+
+def getPrediction(boxes, model):
+    result = []
+    count = 0
+    for image in boxes:
+        img = np.asarray(image)
+        img = img[4:img.shape[0] - 4, 4:img.shape[1] -4]
+        img = cv.resize(img, (28,28))
+        img = img/255
+        img = img.reshape(1,28,28,1)
+        predictions = model.predict(img)
+        classIndex = np.argmax(predictions, axis =-1)
+        probabilityValue = np.amax(predictions)
+        if count ==1:
+            print(classIndex, probabilityValue)
+        count +=1
+        #saving the result
+        if probabilityValue> 0.7:
+            result.append(classIndex[0])
+
+        else:
+            result.append(0)
+    return result
+
